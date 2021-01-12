05b61e00948dfd937c60e0dac49f467e5ae26837
Safwan A. Alqulayti <squlayti@thiqah.sa>
2021-01-12 13:14:28 +0300

    fix

--- src/minio-client/minio-client.module.ts
+++ src/minio-client/minio-client.module.ts
@@ -5,15 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MinioModule } from 'nestjs-minio-client';
 
 @Module({
-  imports:[ConfigModule.forRoot(),
-    MinioModule.register({
-    endPoint: process.env.MINIO_ENDPOINT,
-    port: parseInt(process.env.MINIO_PORT),
-    useSSL: true,
-    accessKey: process.env.MINIO_ACCESSKEY,
-    secretKey: process.env.MINIO_SECRETKEY
-  })
-],
+//   imports:[ConfigModule.forRoot(),
+//     MinioModule.register({
+//     endPoint: process.env.MINIO_ENDPOINT,
+//     port: parseInt(process.env.MINIO_PORT),
+//     useSSL: true,
+//     accessKey: process.env.MINIO_ACCESSKEY,
+//     secretKey: process.env.MINIO_SECRETKEY
+//   })
+// ],
   providers: [MinioClientService],
   controllers: [MinioClientController]
 })
