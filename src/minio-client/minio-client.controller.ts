798ab3e115849491483e14c79d511ca15cd1905e
Safwan A. Alqulayti <squlayti@thiqah.sa>
2021-01-12 12:45:09 +0300

    services added

--- src/minio-client/minio-client.controller.ts
+++ src/minio-client/minio-client.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Delete, Get, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MinioClientService } from './minio-client.service';
 
 @Controller('minio-client')
 export class MinioClientController {
-    constructor(private _minioClientService: MinioClientService) { }
+    constructor(private _minioClientService: MinioClientService
+                ) { }
     @Get('get-all-buckets')
     getAllBucket() {
         return this._minioClientService.getAllBuckets()
@@ -12,17 +13,24 @@ export class MinioClientController {
 
     @Post('upload-opject')
     @UseInterceptors(FileInterceptor('file'))
-    uploudFile(@UploadedFile() file) {
-        return this._minioClientService.putOpject(file)
-    }
-
-    @Post('one-opject')
-    getOpject(@Body('fileName') fileName:string){
-        return this._minioClientService.getOpject(fileName)
+    uploudFile(@UploadedFile() file ,@Body() data) {
+        console.log(file)
+        console.log(data)
+        return this._minioClientService.putOpject(file ,data)
     }
 
     @Delete('remove-opject')
     removeOpject(@Body('fileName') fileName:string){
         return this._minioClientService.removeOpject(fileName)
+    }
+
+    @Post('make-bucket')
+    makeBucket(@Body('bucketName') bucketName:string){
+        return this._minioClientService.makeBucket(bucketName)
+    }
+
+    @Post('bucket-exists')
+    bucketExists(@Body('bucketName') bucketName:string){
+        return this._minioClientService.bucketExists(bucketName)
     }
 }
--- src/minio-client/minio-client.service.ts
+++ src/minio-client/minio-client.service.ts
@@ -3,41 +3,73 @@ import { MinioService } from 'nestjs-minio-client';
 
 @Injectable()
 export class MinioClientService {
-  constructor( 
+  constructor(
     private readonly _minioService: MinioService
-     ) { }
-     public bucket = process.env.MINIO_BUCKET
+  ) { }
+  public bucket = process.env.MINIO_BUCKET
   async getAllBuckets() {
-    return this._minioService.client.listBuckets();
+    let allBuckets = await this._minioService.client.listBuckets();
+    if(allBuckets !== null){
+      return allBuckets
+    }else{
+      let msg = {message: "There is no bucket"}
+      return msg
+    }
   }
 
-  async putOpject(opject) {
-     this._minioService.client.putObject(this.bucket, opject.originalname , opject.buffer ,opject.size, function(err) {
-     if (err) {
-        console.log('Error occured in uploading', err)
-          throw BadRequestException
-    } else {
-       console.log('The file has updluaded successfully')
-     }
-  })
-  }
-
-    getOpject(fileName:string){
-    this._minioService.client.fGetObject(this.bucket, fileName, `./${fileName}`, function(err) {
+  async putOpject(opject,data) {
+    let extension = data.extension
+    let bucketName = data.bucketName
+    let key = await new Date().getTime().toString() ;
+    this._minioService.client.putObject(this.bucket, key , opject.buffer, opject.size, function (err) {
       if (err) {
-        return console.log(err)
+        console.log('Error occured in uploading', err)
+        throw BadRequestException
       }
-      console.log('success')
+    })
+    let fileUrl = `localhost:3000/download/${bucketName}/${key}`
+    return fileUrl
+  }
+  
+//  async presignedUrl(fileName: string) {
+//    let url = await this._minioService.client.
+   
+//    presignedUrl('GET', this.bucket, fileName, 24*60*60, function(err, presignedUrl) {
+//       if (err) {return console.log(err)}
+//       console.log(presignedUrl)
+       
+//     })
+//   }
+
+  removeOpject(fileName: string) {
+    this._minioService.client.removeObject(this.bucket, fileName, function (err) {
+      if (err) {
+        return console.log('Unable to remove object', err)
+      }
+      console.log('the object Removed')
     })
   }
 
-  removeOpject(fileName:string){
-  this._minioService.client.removeObject(this.bucket, fileName, function(err) {
+  makeBucket(bucketName:string){
+    console.log(bucketName)
+  this._minioService.client.makeBucket(bucketName, 'us-east-1', function(err) {
     if (err) {
-      return console.log('Unable to remove object', err)
+       console.log('Error creating bucket.', err)
+       throw BadRequestException
     }
-    console.log('the object Removed')
-   })
+   return console.log('Bucket created successfully in "us-east-1".')
+  })
+}
+bucketExists(bucketName:string){
+  this._minioService.client.bucketExists(bucketName, function(err, exists) {
+    if (err) {
+       console.log(err)
+      throw BadRequestException
+    }
+    if (exists) {
+      return console.log('Bucket exists.')
+    }
+  })
 }
 }
- 
+
